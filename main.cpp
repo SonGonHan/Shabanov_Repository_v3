e20d8832e834ea34b244ab8893fd0fa5b6000855
SonGonHan <miskasabancik@gmail.com>
2023-12-10 13:23:14 +0300

    init_d

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ string init_k(string k) {
 
 string init_d(string d) {
 	bool check = false;
+	cin >> d;
 	string nums[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 	for (string num : nums) {
 		if (num == d) {
