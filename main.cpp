a068eccb22a34a7a5058962d5705042bd0115159
SonGonHan <miskasabancik@gmail.com>
2023-12-10 13:18:02 +0300

    k_plus_d

--- main.cpp
+++ main.cpp
@@ -12,7 +12,24 @@ string init_d(string d) {
 }
 
 int k_plus_d(string k, string d) {
-
+	string nums_k = "0123456789";
+	string nums_d[] = { "1", "2", "3" , "4" , "5" , "6" , "7" , "8" , "9" };
+	bool check_k = true, check_d = false;
+	for (int i = 0; i < k.length(); i++) {
+		if (nums_k.find(k[i]) == string::npos) {
+			check_k = false;
+		}
+	}
+	for (int i = 0; i < 9; i++) {
+		if (nums_d[i] == d) {
+			check_d = true;
+		}
+	}
+	if (check_k && check_d) {
+		return stoi(k + d);
+	}
+	cout << "ERROR";
+	return;
 }
 
 int d_plus_k(string d, string k) {
