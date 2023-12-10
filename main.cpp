b2210b557bfbf70dd8bccd4a0b869e11bf1130c7
SonGonHan <miskasabancik@gmail.com>
2023-12-10 12:56:32 +0300

    main

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string init_k(string k) {
+
+}
+
+string init_d(string d) {
+
+}
+
+int k_plus_d(string k, string d) {
+
+}
+
+int d_plus_k(string d, string k) {
+
+}
+
+int main() {
+
+	return 0;
+}
