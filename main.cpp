57753c691377125002ae9d90ad7d36899989561b
SonGonHan <miskasabancik@gmail.com>
2023-12-10 13:44:31 +0300

    init_k

--- main.cpp
+++ main.cpp
@@ -20,7 +20,19 @@ string init_k(string k) {
 }
 
 string init_d(string d) {
-
+	bool check = false;
+	cin >> d;
+	string nums[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+	for (string num : nums) {
+		if (num == d) {
+			check = true;
+		}
+	}
+	if (check) {
+		return d;
+	}
+	cout << "ERROR";
+	return;
 }
 
 int k_plus_d(string k, string d) {
