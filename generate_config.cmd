4e6181ccf3478a580cd238cf21fad003cfbe0823
SonGonHan <miskasabancik@gmail.com>
2023-12-11 08:07:24 +0300

    not_ready_config

---
+++ generate_config.cmd
@@ -0,0 +1,8 @@
+"C:\Program Files\doxygen\bin\doxygen.exe" -g config
+INPUT = "D:\Учеба\ППО_Курсовая\Shabanov_Repository_v3_23\Shabanov_Repository_v3"
+OUTPUT_DIRECTORY = "D:\Учеба\ППО_Курсовая\Shabanov_Repository_v3_23\Shabanov_Repository_v3"
+PROJECT_BRIEF = "Shabanov_PROJECT"
+PROJECT_NUMBER = "1.0"
+PROJECT_NAME = "Shabanov_PROJECT"
+OUTPUT_LANGUAGE = "Russian"
+INPUT_ENCODING = "Windows-1251"
